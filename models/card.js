4319deeab8f323c67cb897643ce54ce8de7a34b7
Numart <johnn.hotmail@mail.ru>
2023-04-29 21:44:51 +0300

    for review 12

--- models/card.js
+++ models/card.js
@@ -1,41 +1,46 @@
+/* eslint-disable comma-dangle */
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable quotes */
 
 const { Schema, model, mongoose } = require("mongoose");
 const validator = require("validator");
 
-const cardSchema = new Schema({
-  name: {
-    type: String,
-    required: [true, 'Поле "name" обязательное для заполнения'],
-    minlength: [2, 'Поле "name" должно содержать не менее 2 символов'],
-    maxlength: [30, 'Поле "name" должно содержать не более 30 символов'],
-  },
-  link: {
-    type: String,
-    required: [true, 'Поле "link" обязательное для заполнения'],
-    validate: {
-      validator: (v) => validator.isURL(v),
-      message: "Некорректный URL картинки карточки",
+const cardSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Поле "name" обязательное для заполнения'],
+      minlength: [2, 'Поле "name" должно содержать не менее 2 символов'],
+      maxlength: [30, 'Поле "name" должно содержать не более 30 символов'],
     },
-  },
-  owner: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "user",
-    required: [true, 'Поле "owner" обязательное для заполнения'],
-  },
-  likes: [
-    {
-      type: mongoose.SchemaTypes.ObjectId,
+    link: {
+      type: String,
+      required: [true, 'Поле "link" обязательное для заполнения'],
+      validate: {
+        validator: (v) => validator.isURL(v),
+        message: "Некорректный URL картинки карточки",
+      },
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
       ref: "user",
-      default: {},
+      required: [true, 'Поле "owner" обязательное для заполнения'],
     },
-  ],
-  createdAt: {
-    type: Date,
-    default: Date.now(),
+    likes: [
+      {
+        type: mongoose.SchemaTypes.ObjectId,
+        ref: "user",
+        default: {},
+      },
+    ],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
   },
-  versionKey: false,
-});
+  {
+    versionKey: false,
+  }
+);
 
 module.exports = model("card", cardSchema);
--- models/user.js
+++ models/user.js
@@ -1,31 +1,36 @@
+/* eslint-disable comma-dangle */
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable quotes */
 
 const { Schema, model } = require("mongoose");
 const validator = require("validator");
 
-const userSchema = new Schema({
-  name: {
-    type: String,
-    required: [true, 'Поле "name" обязательное для заполнения'],
-    minlength: [2, 'Поле "name" должно содержать не менее 2 символов'],
-    maxlength: [30, 'Поле "name" должно содержать не более 30 символов'],
-  },
-  about: {
-    type: String,
-    required: [true, 'Поле "about" обязательное для заполнения'],
-    minlength: [2, 'Поле "about" должно содержать не менее 2 символов'],
-    maxlength: [30, 'Поле "about" должно содержать не более 30 символов'],
-  },
-  avatar: {
-    type: String,
-    required: [true, 'Поле "avatar" обязательное для заполнения'],
-    validate: {
-      validator: (v) => validator.isURL(v),
-      message: "Некорректный URL картинки для аватара",
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Поле "name" обязательное для заполнения'],
+      minlength: [2, 'Поле "name" должно содержать не менее 2 символов'],
+      maxlength: [30, 'Поле "name" должно содержать не более 30 символов'],
+    },
+    about: {
+      type: String,
+      required: [true, 'Поле "about" обязательное для заполнения'],
+      minlength: [2, 'Поле "about" должно содержать не менее 2 символов'],
+      maxlength: [30, 'Поле "about" должно содержать не более 30 символов'],
+    },
+    avatar: {
+      type: String,
+      required: [true, 'Поле "avatar" обязательное для заполнения'],
+      validate: {
+        validator: (v) => validator.isURL(v),
+        message: "Некорректный URL картинки для аватара",
+      },
     },
   },
-  versionKey: false,
-});
+  {
+    versionKey: false,
+  }
+);
 
 module.exports = model("user", userSchema);

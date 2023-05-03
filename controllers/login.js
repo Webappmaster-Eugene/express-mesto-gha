979a6c0ebff7628ba8dfd1c1960737dba408c0af
Numart <johnn.hotmail@mail.ru>
2023-05-03 13:37:03 +0300

    feat: release 2.0.2

--- controllers/login.js
+++ controllers/login.js
@@ -16,7 +16,7 @@ const User = require("../models/user");
 const login = async (req, res) => {
   try {
     const { email, password } = req.body;
-    const user = await User.findOne({ email });
+    const user = await User.findOne({ email }).select("+password");
 
     if (!user) {
       return res
